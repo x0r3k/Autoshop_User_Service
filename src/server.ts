41d6ef1a15015ec1605a993a1ab11b2229d63757
Dolhopolov, Kyrylo <k.dolhopolov@telesens.ua>
2021-07-02 19:27:37 +0300

    remove unused modules, variables and imports

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express, { ErrorRequestHandler } from 'express';
+import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import helmet from 'helmet';
 import config from './config/serviceConfig';
-import { errorHandler, httpErrorHandler, throwHttpError, createHttpError, MAIN_ERROR_CODES } from './services/errorHandling';
-import createError from 'http-errors';
+import {
+  errorHandler, httpErrorHandler, createHttpError, MAIN_ERROR_CODES,
+} from './services/errorHandling';
 // import errorHandler from './services/errorHandling/errorHandler';
 
 require('dotenv').config();
@@ -25,11 +26,9 @@ app.get(baseApiUrl, (req, res) => {
   res.send('Hello world');
 });
 
-app.use('*', (req, res, next) => {
-  // return next(createHttpError(MAIN_ERROR_CODES.NOT_FOUND));
-  return next(new Error('test'));
-  // res.status(404).send('Page not found');
-});
+app.use('*', (req, res, next) => next(createHttpError(MAIN_ERROR_CODES.NOT_FOUND)));
+// next(new Error('test')),
+// res.status(404).send('Page not found');
 
 app.use(httpErrorHandler);
 
